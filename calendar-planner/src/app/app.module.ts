c76169be74e8420c5c36a5bff38cacfb90323951
Alesia999 <73301282+Alesia999@users.noreply.github.com>
2022-05-06 10:08:33 +0200

    delete nav

--- calendar-planner/src/app/app.module.ts
+++ calendar-planner/src/app/app.module.ts
@@ -1,16 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { CalendarComponent } from './calendar/calendar.component';
-import { NavigationComponent } from './navigation/navigation.component';
 import { TasksFormComponent } from './tasks-form/tasks-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CalendarComponent,
-    NavigationComponent,
+    CalendarComponent,    
     TasksFormComponent
   ],
   imports: [
